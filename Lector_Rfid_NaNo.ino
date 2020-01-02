ff9d49ae7ef7ed4627979122c016fa555127660a
nahueldebellis <debellisnahuel@gmail.com>
2020-01-02 03:10:14 -0300

    quit comments and move includes and defines

--- Lector_Rfid_NaNo.ino
+++ Lector_Rfid_NaNo.ino
@@ -1,20 +1,17 @@
-#include <SPI.h>
-#include <SD.h>
-#include <MFRC522.h>
-#include "tag.h"
-#include "sd_manager.h"
-
 #define SDA 10
 #define SDA_2 8
 #define RST 9
 #define RELE 4
 #define SD_CARD 3
 
-
-//keys[4] = "197 239 203 45";
-
+#include <SPI.h>
+#include <SD.h>
+#include <MFRC522.h>
+#include "tag.h"
+#include "sd_manager.h"
 MFRC522 rfid_1(SDA, RST); 
-MFRC522 rfid_2(SDA_2, RST);
+MFRC522 rfid_2(SDA_2, RST); ///las creo aca para poder acceder a los lectores desde teclado.h y poder hacer el agregado de tags mas facil
+#include "teclado.h"
 
 void setup() {
   Serial.begin(9600);
@@ -33,7 +30,7 @@ void setup() {
 
 void loop(){
   //today = now();
-  //new_input();
+  new_key();
   if(rfid_1.PICC_IsNewCardPresent()){
      rfid_1.PICC_ReadCardSerial();
      check_incomin_tag(rfid_1);
@@ -56,48 +53,3 @@ void check_incomin_tag (MFRC522 rfid){
       digitalWrite(RELE, LOW);
     }
 }
-
-/*String read_incomin_tag(){
-  if(rfid_1.PICC_IsNewCardPresent()){
-     rfid_1.PICC_ReadCardSerial();
-     return String(rfid_1.uid.uidByte[0])+" "+String(rfid_1.uid.uidByte[1])+" "+String(rfid_1.uid.uidByte[2])+ " "+String(rfid_1.uid.uidByte[3]);
-  }
-  else if( rfid_2.PICC_IsNewCardPresent()){
-    rfid_2.PICC_ReadCardSerial();
-    return String(rfid_2.uid.uidByte[0])+" "+String(rfid_2.uid.uidByte[1])+" "+String(rfid_2.uid.uidByte[2])+ " "+String(rfid_2.uid.uidByte[3]);
-  }
-}
-
-
-void save_incomin_tag(){
-  Serial.println("enter tag: ");
-  String uidString = read_incomin_tag();
-  save_new_record(uidString, true);
-}
-
-void delete_incomin_tag(){
-  String uidString = read_incomin_tag();
-  //delete_record(uidString, false);
-}
-
-char read_incomin_keyboard(){
-  char action;
-  Serial.println("enter action: ");
- if (Serial.available() > 0) 
-    action = Serial.read();
- else
-    return 'f';
- return action;
-}
-
-bool new_input(){
-  char a = 'a';//read_incomin_keyboard();
-  switch(a){
-    case 'a': save_incomin_tag();
-      break;
-    case 'd': delete_incomin_tag();
-       break;
-    default: return false;
-  }
-  return true;
-}*/
